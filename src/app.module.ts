fa43558e871e8231680324ec53b597f04c5667e5
Ro Lee <rolee0429@gmail.com>
2020-09-18 11:06:30 -0700

    Add Kafka + Eureka

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { EurekaService } from './eureka/eureka.service';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot()
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, EurekaService],
 })
 export class AppModule {}
